166b206e7e28a1a7bc9589b9ad56abe31bcd4467
Jason Fujii <Jasonfujii@yahoo.com>
2018-10-02 21:41:11 -0700

    Monster Attacks

---
+++ JBYuGiOh/Assets/Scripts/Monster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+	public int atk;
+	public int def;
+	public int level;
+	public string type;
+	public string name;
+	public Monster enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")){
+			this.Attack(enemy);
+		}
+	}
+
+	//Calculation method for Battle Phase
+	void Attack(Monster m)
+	{
+		if(atk > m.atk){
+			//attacked monster dies
+			//destroy game object
+			GameObject.Destroy(m.gameObject.transform.GetChild(0).gameObject);
+
+		}
+		else if(atk < m.atk){
+			//attacker dies
+			//destroy game object
+			GameObject.Destroy(this.gameObject.transform.GetChild(0).gameObject);
+		}
+		else{
+			//they both die	
+			//destroy both
+			GameObject.Destroy(m.gameObject.transform.GetChild(0).gameObject);
+			GameObject.Destroy(this.gameObject.transform.GetChild(0).gameObject);
+		}
+	}
+}
